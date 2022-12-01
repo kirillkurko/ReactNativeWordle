be00df65e8b083fa65590f7fa9fc85785836e655
Kirill Kurko <kirillkurko2609@gmail.com>
2022-12-01 19:36:44 +0300

    Project dependencies updated

---
+++ src/navigation/index.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Screen } from './Screen';
+import { Game } from '../screens';
+import { RootStackParamList } from './RootStackParamList';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={Screen.Game}>
+        <Stack.Group screenOptions={{ headerShown: false }}>
+          <Stack.Screen name={Screen.Game} component={Game} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Game = () => {
+  return (
+    <View>
+      <Text>Game</Text>
+    </View>
+  );
+};
+
+export default Game;
